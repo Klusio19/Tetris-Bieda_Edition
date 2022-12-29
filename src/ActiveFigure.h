a0a3f1f21e17936c895d489742491e3860b592a3
Klusio19 <96704102+Klusio19@users.noreply.github.com>
2022-12-29 21:10:13 +0100

    Dodanie fullscreen'a, dodanie renderowania tła, lini i napisów, niezależnie od rozmiaru okna, czy bloku.

--- src/ActiveFigure.h
+++ src/ActiveFigure.h
@@ -7,6 +7,20 @@ const int BOARD_WIDTH = 10;
 // Wymiary planszy do gry (w klockach)
 const int BOARD_HEIGHT = 20;
 
+// Wymiary pojedynczego klocka
+const int BLOCK_SIZE = 54;
+
+// Wymiary ca³ego okna
+//const int WINDOW_WIDTH = (BOARD_WIDTH * BLOCK_SIZE + 200);
+const int WINDOW_WIDTH = 1920;
+// Wymiary ca³ego okna
+//const int WINDOW_HEIGHT = (BOARD_HEIGHT * BLOCK_SIZE);
+const int WINDOW_HEIGHT = 1080;
+
+//Wspó³rzêdna X, dla której t³o(bia³y prostok¹t) wyrysuje siê na œrodku ekranu.
+//Niezale¿na od rozmiaru okna oraz wielkoœci klocka.
+const int CENTER = (WINDOW_WIDTH / 2) - (BOARD_WIDTH * BLOCK_SIZE / 2);
+
 class ActiveFigure
 {
 private:
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,7 @@
-#include <SFML/Graphics.hpp>
+#include "SFML/Graphics.hpp"
 #include "Block.h"
 #include "ActiveFigure.h"
-#include <iostream>
-
-// Wymiary pojedynczego klocka
-const int BLOCK_SIZE = 40;
-
-// Wymiary całego okna
-const int WIDTH = BOARD_WIDTH * BLOCK_SIZE + 200;
-// Wymiary całego okna
-const int HEIGHT = BOARD_HEIGHT * BLOCK_SIZE;
+#include "Game.h"
 
 
 
@@ -18,32 +10,32 @@ const float FALL_SPEED = 0.5f;
 
 int main()
 {
-    srand(time(0));
+    srand(unsigned int(time(0)));
 
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Tetris Bieda Edition");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Tetris Bieda Edition", sf::Style::Fullscreen);
+    sf::Event event;
+    sf::Clock clock;
 
-    float currentTime = 0.0f;
-    float previousTime = 0.0f;
+    //Czcionka
+    sf::Font open_sans;
+    open_sans.loadFromFile("Dependencies\\Fonts\\static\\OpenSans\\OpenSans-Medium.ttf");
 
-    // Zmienna przechowująca aktualnie spadający klocek
+    // Zmienna przechowująca aktualnie spadającą figurę
     ActiveFigure activeFigure;
 
     // Wektor przechowujący klocki pozostające na planszy po opadnięciu
     std::vector<Block> fixedFigures;
 
-    //Czcionka
-    sf::Font open_sans;
-    //open_sans.loadFromFile("D:\\Dokumenty\\Projekty_Visual\\C++\\Tetris-Bieda_edition\\Dependencies\\Fonts\\static\\OpenSans\\OpenSans-Medium.ttf");
-    open_sans.loadFromFile("Dependencies\\Fonts\\static\\OpenSans\\OpenSans-Medium.ttf");
+    float currentTime = 0.0f;
+    float previousTime = 0.0f;
 
-
-    sf::Clock clock;
     static int points = 0;
+
+
 
     while (window.isOpen())
     {
-        // Obsługa zdarzeń, naciśnięć klawiszy
-        sf::Event event;
+        // Obsługa naciśnięć klawiszy
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed) window.close();
@@ -53,34 +45,20 @@ int main()
                 {
                     window.close();
                 }
-
                 if (event.key.code == sf::Keyboard::Left)
                 {
                     if (activeFigure.canMove(-1, 0, fixedFigures)) activeFigure.move(-1, 0);
                 }
-
                 if (event.key.code == sf::Keyboard::Right)
                 {
                     if (activeFigure.canMove(1, 0, fixedFigures)) activeFigure.move(1, 0);
                 }
-
                 if (event.key.code == sf::Keyboard::Down)
                 {
                     if (activeFigure.canMove(0, 1, fixedFigures)) activeFigure.move(0, 1);
                 }
-
-                if (event.key.code == sf::Keyboard::Q)
-                {
-                    //if (activeFigure.canMove(0, 0, fixedFigures)) 
-                    activeFigure.rotateClockwise();
-                }
-
-                if (event.key.code == sf::Keyboard::E)
-                {
-                    //if (activeFigure.canMove(0, 0, fixedFigures)) 
-                    activeFigure.rotateCounterclockwise();
-                }
-
+                if (event.key.code == sf::Keyboard::Q) activeFigure.rotateClockwise();
+                if (event.key.code == sf::Keyboard::E) activeFigure.rotateCounterclockwise();
             }
         }
 
@@ -96,6 +74,7 @@ int main()
                 if (activeFigure.getY() == 0)
                 {
                     clock.restart();
+                    break;
                 }
                 // Jeśli klocek nie może opaść, dodajemy go do wektora  klocków pozostających na planszy
                 for (Block& a : activeFigure.getBlocks())
@@ -124,7 +103,7 @@ int main()
                         }
                     }
 
-                    if (fullRow)
+                    if (fullRow == true)
                     {
                         // Jeśli znaleziono pełny wiersz, usuwamy go i przesuwamy wszystkie klocki powyżej o jedną pozycję w dół
                         fixedFigures.erase(std::remove_if(fixedFigures.begin(), fixedFigures.end(), [&](Block& block) { return block.getY() == i; }), fixedFigures.end());
@@ -133,7 +112,7 @@ int main()
                         for (Block& block : fixedFigures)
                         {
                             if (block.getY() < i)
-                                block.setY(block.getY() + 1); //TODO sprawdzić, zwiększenie m_y o jeden
+                                block.setY(block.getY() + 1);
                         }
                     }
                 }
@@ -146,52 +125,39 @@ int main()
 
         // Rysowanie planszy gry
         window.clear();
+
+        //Białe tło
+        sf::RectangleShape whiteBackground(sf::Vector2f(BOARD_WIDTH * BLOCK_SIZE, BOARD_HEIGHT * BLOCK_SIZE));
+        whiteBackground.setPosition(CENTER, 0);
+        whiteBackground.setFillColor(sf::Color::White);
+        window.draw(whiteBackground);
+
+        //Kolorowy bok
+        sf::RectangleShape colorSide(sf::Vector2f(CENTER, WINDOW_HEIGHT));
+        colorSide.setPosition(10 * BLOCK_SIZE + CENTER, 0);
+        colorSide.setFillColor(sf::Color(153, 204, 255));
+        window.draw(colorSide);
+        colorSide.setPosition(0, 0);
+        window.draw(colorSide);
+
         for (int i = 0; i < BOARD_WIDTH; i++)
         {
             for (int j = 0; j < BOARD_HEIGHT; j++)
             {
                 //Linie poziome
-                sf::RectangleShape horizontalLines(sf::Vector2f(WIDTH, 1));
-                horizontalLines.setPosition(0, j * BLOCK_SIZE);
+                sf::RectangleShape horizontalLines(sf::Vector2f(BLOCK_SIZE * BOARD_WIDTH, 1.0f));
+                horizontalLines.setPosition(CENTER, float(j * BLOCK_SIZE));
                 horizontalLines.setFillColor(sf::Color::Black);
 
                 //Linie pionowe
-                sf::RectangleShape verticalLines(sf::Vector2f(HEIGHT, 1));
+                sf::RectangleShape verticalLines(sf::Vector2f(BLOCK_SIZE * BOARD_HEIGHT, 1.0f));
                 verticalLines.rotate(90);
-                verticalLines.setPosition(i * BLOCK_SIZE, 0);
+                verticalLines.setPosition(float(CENTER + (i * BLOCK_SIZE)), 0.0f);
                 verticalLines.setFillColor(sf::Color::Black);
 
-                //Białe tło
-                sf::RectangleShape whiteBackground(sf::Vector2f(BLOCK_SIZE, BLOCK_SIZE));
-                whiteBackground.setPosition(i * BLOCK_SIZE, j * BLOCK_SIZE);
-                whiteBackground.setFillColor(sf::Color::White);
-
-                //Kolorowy prawy bok
-                sf::RectangleShape colorRightSide(sf::Vector2f(200, HEIGHT));
-                colorRightSide.setPosition(400, 0);
-                colorRightSide.setFillColor(sf::Color(153, 204, 255));
-
-                window.draw(whiteBackground);
                 window.draw(verticalLines);
                 window.draw(horizontalLines);
-                window.draw(colorRightSide);
             }
-        }
-        // Rysowanie klocków pozostających na planszy
-        for (Block block : fixedFigures)
-        {
-            sf::RectangleShape rectangle(sf::Vector2f(BLOCK_SIZE, BLOCK_SIZE));
-            rectangle.setPosition(block.getX() * BLOCK_SIZE, block.getY() * BLOCK_SIZE);
-            rectangle.setFillColor(block.getColor());
-            window.draw(rectangle);
-        }
-        // Rysowanie aktualnie spadającego klocka
-        for (Block block : activeFigure.getBlocks())
-        {
-            sf::RectangleShape rectangle(sf::Vector2f(BLOCK_SIZE, BLOCK_SIZE));
-            rectangle.setPosition((activeFigure.getX() + block.getX())* BLOCK_SIZE, (activeFigure.getY() + block.getY())* BLOCK_SIZE);
-            rectangle.setFillColor(block.getColor());
-            window.draw(rectangle);
         }
 
         //Rysowanie tekstu z punktami
@@ -199,10 +165,31 @@ int main()
         pointsNumber.setFont(open_sans);
         std::string s_points = "Punkty:\n     " + std::to_string(points);
         pointsNumber.setString(s_points);
-        pointsNumber.setPosition(410, 350);
+        pointsNumber.setPosition(CENTER/2 - 50, (WINDOW_HEIGHT/2) - 100);
         pointsNumber.setCharacterSize(50);
         pointsNumber.setFillColor(sf::Color::Red);
         window.draw(pointsNumber);
+        pointsNumber.setPosition((WINDOW_WIDTH)/2 + (BOARD_WIDTH*BLOCK_SIZE)/2 + 200, (WINDOW_HEIGHT / 2) - 100);
+        window.draw(pointsNumber);
+
+        // Rysowanie klocków pozostających na planszy
+        for (Block block : fixedFigures)
+        {
+            sf::RectangleShape rectangle(sf::Vector2f(BLOCK_SIZE, BLOCK_SIZE));
+            rectangle.setPosition(float(block.getX() * BLOCK_SIZE), float(block.getY() * BLOCK_SIZE));
+            rectangle.setFillColor(block.getColor());
+            window.draw(rectangle);
+        }
+        // Rysowanie aktualnie spadającego klocka
+        for (Block block : activeFigure.getBlocks())
+        {
+            sf::RectangleShape rectangle(sf::Vector2f(BLOCK_SIZE, BLOCK_SIZE));
+            rectangle.setPosition(float((activeFigure.getX() + block.getX())* BLOCK_SIZE), float((activeFigure.getY() + block.getY())* BLOCK_SIZE));
+            rectangle.setFillColor(block.getColor());
+            window.draw(rectangle);
+        }
+
+        
 
         window.display();
     }
